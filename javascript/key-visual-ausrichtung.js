f58b4f8f58c1e997ab6523ea3c36290133ddfe9b
AndrePascalWerthwein <andre.werthwein@gmx.de>
2019-06-23 22:23:58 +0200

    Optimierung, Responsive Design
    _ Optimierung und Responsive Design des Key-Visuals

--- javascript/key-visual-ausrichtung.js
+++ javascript/key-visual-ausrichtung.js
@@ -1,53 +1,26 @@
-var FensterHoehe = window.innerHeight;
-var KeyVisualHoehe = document.querySelector('#KeyVisual > img').offsetHeight;
-
-var KeyVisual = document.querySelector('#KeyVisual > img');
-
-function KeyVisualAusrichtung() {
-  // Ist das Key-Visual breiter, als der im Fenster verfügbare Platz, dann wird das Key-Visual mittig ausgerichtet, um die optimale und vollflächige Darstellung des Key-Visuals zu garaniteren.
-  if (KeyVisualHoehe < FensterHoehe) {
-    // Verschiebung um die Hälfte der Breiten-Differenz zwischen dem Key-Visual und dem im Fenster verfügbaren Platzes
-    var KeyVisualVerschiebung = ((FensterHoehe - KeyVisualHoehe) / 2);
-
-    KeyVisual.style.marginTop = KeyVisualVerschiebung + "px";
-  } else if (KeyVisualHoehe > FensterHoehe) {
-    var KeyVisualVerschiebung = ((KeyVisualHoehe - FensterHoehe) / 2) * (-1);
-
-    KeyVisual.style.marginTop = KeyVisualVerschiebung + "px";
-  }
-}
-
-KeyVisualAusrichtung();
-
-window.addEventListener('resize', function() {
-  FensterBreite = window.innerHeight;
-  KeyVisualAusrichtung();
-});
-
+// Bei der Veränderung der Ausrichtung des Bildschirms sollte entsprechend reagiert werden können
 window.addEventListener('orientationchange', function() {
-  FensterBreite = window.innerHeight;
-  KeyVisualAusrichtung();
+  KeyVisualDynamik();
 });
 
-// Handelt es sich um ein Mobiles Endgerät, muss eventuelle das Key-Visual gegen ein Hochformat getauscht werden
-if (navigator.userAgent.match(/Android/i) || navigator.userAgent.match(/webOS/i) || navigator.userAgent.match(/iPhone/i) || navigator.userAgent.match(/iPad/i) || navigator.userAgent.match(/iPod/i) || navigator.userAgent.match(/BlackBerry/i) || navigator.userAgent.match(/Windows Phone/i)) {
-  var FensterBreite = window.innerWidth;
-  if (FensterHoehe > FensterBreite) {
-    KeyVisual.src = "medien/keyvisual-hochformat.jpg";
-    // KeyVisual.style.width = "auto";
-    KeyVisual.style.height= "calc(100vh - (2 * 94.8px))";
+// Beim Laden der Website, sollte zuerst die Bildschirm-Ausrichtung abgefragt werden
+window.addEventListener('load', function() {
+  KeyVisualDynamik();
+});
 
-    KeyVisual.style.marginTop = 94.8 + "px";
-    var FensterBreite = window.innerWidth;
-    console.log("Fenstergröße: " + FensterBreite);
-    KeyVisual = document.querySelector('#KeyVisual > img');
-    console.log("Key-Visual-Größe: " + KeyVisual.clientWidth);
+function KeyVisualDynamik() {
+  var KeyVisual = document.querySelector('#KeyVisual');
+  // Prüfung: Handelt es sich um ein mobiles Endgerät?
+  if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|ipad|iris|kindle|Android|Silk|lge |maemo|midp|mmp|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i.test(navigator.userAgent) || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(navigator.userAgent.substr(0,4))) {
+    var ViewportAusrichtung = window.orientation;
+    // console.log(ViewportAusrichtung);
 
-    if (FensterBreite < KeyVisual.offsetWidth) {
-      var Verschiebung = ((KeyVisual.offsetWidth - FensterBreite) / 2) * (-1);
-
-      KeyVisual.style.marginLeft = Verschiebung + "px";
+    if (ViewportAusrichtung === 90 || ViewportAusrichtung === -90) {
+      // Wird das mobile Endgeräte im "Landscape"-Format bedient?
+      KeyVisual.style.backgroundImage = "url('./medien/keyvisual-querformat.jpg')"
+    } else {
+      // Wird das mobile Endgeräte im "Portrait"-Format bedient?
+      KeyVisual.style.backgroundImage = "url('./medien/keyvisual-hochformat.jpg')"
     }
-    // KeyVisualAusrichtung();
   }
 }
--- javascript/navigation/responsive-navigation.js
+++ javascript/navigation/responsive-navigation.js
@@ -40,28 +40,30 @@ ResponsiveNavigationBurger.addEventListener('click', function(e) {
   }
 });
 
-// Zusätzliche Interatkion für das Logo, Erhöhte User-Experience
-ResponsiveNavigationLogo.addEventListener('click', function(e) {
-  e.preventDefault();
+if (ResponsiveNavigationLogo != undefined) {
+  // Zusätzliche Interatkion für das Logo, Erhöhte User-Experience
+  ResponsiveNavigationLogo.addEventListener('click', function(e) {
+    e.preventDefault();
 
-  if (ResponsiveNavigationAnzeige === false) {
-    // Wird das Navigationsmenü geöffnet/geschlossen, dann sollte an den Kopf des Dokument gescrollt werden, um ggf. Menü auch sichtbar zu machen.
-    Scrolling();
-    // Animation des Hamburger-Menüs, vom Hamburger-Menü zum Schliessen-Kreuz
-    ResponsiveNavigationBurger.classList.add('ResponsiveNavigationKreuz');
-    // Einblenden des Navigationsmenü
-    ResponsiveNavigation.classList.add('ResponsiveNavigationEinfahren');
-    ResponsiveNavigationAnzeige = true;
-  } else {
-    // Wird das Navigationsmenü geöffnet/geschlossen, dann sollte an den Kopf des Dokument gescrollt werden, um ggf. Menü auch sichtbar zu machen.
-    Scrolling();
-    // Animation des Hamburger-Menüs, vom Schliessen-Kreuz zum Hamburger-Menü
-    ResponsiveNavigationBurger.classList.remove('ResponsiveNavigationKreuz');
-    // Ausblenden des Navigationsmenü
-    ResponsiveNavigation.classList.remove('ResponsiveNavigationEinfahren');
-    ResponsiveNavigationAnzeige = false;
-  }
-});
+    if (ResponsiveNavigationAnzeige === false) {
+      // Wird das Navigationsmenü geöffnet/geschlossen, dann sollte an den Kopf des Dokument gescrollt werden, um ggf. Menü auch sichtbar zu machen.
+      Scrolling();
+      // Animation des Hamburger-Menüs, vom Hamburger-Menü zum Schliessen-Kreuz
+      ResponsiveNavigationBurger.classList.add('ResponsiveNavigationKreuz');
+      // Einblenden des Navigationsmenü
+      ResponsiveNavigation.classList.add('ResponsiveNavigationEinfahren');
+      ResponsiveNavigationAnzeige = true;
+    } else {
+      // Wird das Navigationsmenü geöffnet/geschlossen, dann sollte an den Kopf des Dokument gescrollt werden, um ggf. Menü auch sichtbar zu machen.
+      Scrolling();
+      // Animation des Hamburger-Menüs, vom Schliessen-Kreuz zum Hamburger-Menü
+      ResponsiveNavigationBurger.classList.remove('ResponsiveNavigationKreuz');
+      // Ausblenden des Navigationsmenü
+      ResponsiveNavigation.classList.remove('ResponsiveNavigationEinfahren');
+      ResponsiveNavigationAnzeige = false;
+    }
+  });
+}
 
 // Vertiakels Scrolling, Interaktion der Responsive Navigation
 var ScrollingButton = document.querySelectorAll('.ScrollingButton');
